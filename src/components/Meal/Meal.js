6f487b24554e5afbedd9bfa0fc2409276dab7da0
Saioa <saioa@hodeidesign.com>
2021-08-08 18:28:59 +0200

    Formateo del componente Meal

--- src/components/Meal/Meal.js
+++ src/components/Meal/Meal.js
@@ -1,20 +1,36 @@
-import './Meal.scss';
+import { useState } from 'react'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAngleRight, faAngleLeft, faPen, faTrashAlt } from '@fortawesome/free-solid-svg-icons'
+
+import './Meal.scss'
 
 const Meal = ({ id }) => {
+	const [isSubmenuOpen, setIsSubmenuOpen] = useState(true)
+	const iSubmenuOpen = <FontAwesomeIcon icon={faAngleRight} />
+	const iSubmenuClose = <FontAwesomeIcon icon={faAngleLeft} />
+	const iEdit = <FontAwesomeIcon icon={faPen} />
+	const iRemove = <FontAwesomeIcon icon={faTrashAlt} />
+
+	const handleSubmenu = () => {
+		setIsSubmenuOpen(!isSubmenuOpen)
+	}
 
 	return (
 		<article className="meal">
 			<div className="meal__content">
-				<h2 className="meal__title">Melón con jamón</h2>
 				<span className="meal__type">Desayuno</span>
 				<span className="meal__time">08:34</span>
+				<h2 className="meal__title">Melón con jamón y muchas más cosas para comer</h2>
 			</div>
-			<nav className="meal__actions">
-				<button type="button">E</button>
-				<button type="button">B</button>
-			</nav>
+			<button type="button" className="meal__submenuOpen" onClick={handleSubmenu}>{iSubmenuOpen}</button>
+			<div className={isSubmenuOpen ? 'meal__actions meal__actions--open' : 'meal__actions'}>
+				<button type="button" className="meal__submenuClose" onClick={handleSubmenu}>{iSubmenuClose}</button>
+				<button type="button" className="meal__edit">{iEdit}</button>
+				<button type="button" className="meal__remove">{iRemove}</button>
+			</div>
 		</article>
-	);
-};
+	)
+}
 
-export default Meal;
+export default Meal
