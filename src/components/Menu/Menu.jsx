eaeea18392f24ab8042fe87e86af7291ebb4e5cb
Saioa <saioa@hodeidesign.com>
2021-08-13 16:31:14 +0200

    Create Menu component

---
+++ src/components/Menu/Menu.jsx
@@ -0,0 +1,15 @@
+import './Menu.scss'
+
+const Menu = () => {
+	return (
+		<nav className="menu">
+			<a href="../">Inicio</a>
+			<a href="../ingredients">Ingredientes</a>
+			<a href="../ingredientTypes">Tipos de ingredientes</a>
+			<a href="../plates">Platos</a>
+			<a href="../plateTypes">Tipos de platos</a>
+		</nav>
+	)
+}
+
+export default Menu
