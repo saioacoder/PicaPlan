6aa1d198b39294c09179edee60c9905b30958ffe
Saioa <saioa@hodeidesign.com>
2021-08-14 10:01:06 +0200

    Add the items loop to Diary page

--- src/pages/diary/diary.jsx
+++ src/pages/diary/diary.jsx
@@ -1,27 +1,30 @@
+import { useState, useEffect } from 'react'
+
+import { loadList } from '../../logic/shared'
+
 import ButtonAddItem from "../../components/ButtonAddItem/ButtonAddItem.jsx"
-import DiaryHeader from "../../components/DiaryHeader/DiaryHeader.jsx"
+import DaysNav from "../../components/DaysNav/DaysNav.jsx"
 import Meal from "../../components/Meal/Meal.jsx"
 import PageLayout from '../../components/PageLayout/PageLayout.jsx'
 
 const Diary = () => {
+
+	const [selectedDay, setSelectedDay] = useState({})
+	const [days, setDays] = useState([])
+
+	useEffect(() => {
+		loadList('days', setDays)
+	}, [])
+
 	return (
 		<PageLayout>
-			<DiaryHeader type="day"/>
-			<section className="container">
-				<Meal id="123123213" />
-				<Meal id="123123213" />
-				<Meal id="123123213" />
-				<Meal id="123123213" />
-				<Meal id="123123213" />
-				<Meal id="123123213" />
-				<Meal id="123123213" />
-				<Meal id="123123213" />
-				<Meal id="123123213" />
-				<Meal id="123123213" />
-				<Meal id="123123213" />
-				<Meal id="123123213" />
-				<Meal id="123123213" />
-			</section>
+			<DaysNav
+				selectedDay={selectedDay}
+				onChangeDay={setSelectedDay}
+			/>
+			{days.map(({ id, name }) => {
+				return <Meal key={id} />
+			})}
 			<ButtonAddItem type="day" />
 		</PageLayout>
 	)
