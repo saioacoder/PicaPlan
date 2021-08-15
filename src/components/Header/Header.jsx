179598f23be313be5f79b027872b11ff8c0d1059
Saioa <saioa@hodeidesign.com>
2021-08-15 17:04:49 +0200

    Add to header Title and menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,38 @@
-import './Header.scss'
+import { useState } from 'react';
 
-const Header = () => {
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
+
+import './Header.scss';
+
+const Header = ({ pageTitle }) => {
+	const [isMenuOpen, setIsMenuOpen] = useState(false);
+	const iMenu = <FontAwesomeIcon icon={faBars} />;
+	const iClose = <FontAwesomeIcon icon={faTimes} />;
 	return (
 		<header className="pageHeader">
-			<nav className="pageHeader__menu">
+			<h1 className="pageHeader__title">{pageTitle}</h1>
+			<button
+				type="button"
+				className="pageHeader__menuOpen"
+				onClick={() => setIsMenuOpen(true)}
+			>
+				{iMenu}
+			</button>
+			<nav
+				className={
+					isMenuOpen
+						? 'pageHeader__menu pageHeader__menu--open'
+						: 'pageHeader__menu'
+				}
+			>
+				<button
+					type="button"
+					className="pageHeader__menuClose"
+					onClick={() => setIsMenuOpen(false)}
+				>
+					{iClose}
+				</button>
 				<a href="../">Inicio</a>
 				<a href="../ingredients">Ingredientes</a>
 				<a href="../ingredientTypes">Tipos de ingredientes</a>
@@ -11,7 +40,7 @@ const Header = () => {
 				<a href="../plateTypes">Tipos de platos</a>
 			</nav>
 		</header>
-	)
-}
+	);
+};
 
-export default Header
+export default Header;
