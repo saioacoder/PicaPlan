1cad52ef39906002861526f985b13be2e37e119f
Saioa <saioa@hodeidesign.com>
2021-08-15 17:08:29 +0200

    Add more functions to shared functions file

--- src/logic/shared.js
+++ src/logic/shared.js
@@ -1,16 +1,31 @@
-import { getCollection } from '../services/data'
+import { getCollection, getDocumentById } from '../services/data';
 
 async function getList(name) {
 	try {
-		const data = await getCollection(name)
-		return data ? data : null
+		const data = await getCollection(name);
+		return data ? data : [];
 	} catch (error) {
-		console.log("getList Error:", error)
-		return null
+		console.log('getList Error:', error);
+		return [];
+	}
+}
+
+async function getListItem(name, id) {
+	try {
+		const data = await getDocumentById(name, id);
+		return data ? data : [];
+	} catch (error) {
+		console.log('getListItem Error:', error);
+		return [];
 	}
 }
 
 export const loadList = async (listName, updateFunction) => {
-	const result = await getList(listName)
-	result && updateFunction(result)
-}
+	const result = await getList(listName);
+	updateFunction(result ? result : []);
+};
+
+export const loadItem = async (name, id, updateFunction) => {
+	const result = await getListItem(name, id);
+	updateFunction(result ? result : {});
+};
