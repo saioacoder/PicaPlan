f4897d8f384db447149c90e2dbe8c690c3a7a711
Saioa <saioacoder@gmail.com>
2021-08-21 16:39:45 +0200

    Created style and design logic to SelectField component

--- src/components/SelectField/SelectField.jsx
+++ src/components/SelectField/SelectField.jsx
@@ -1,14 +1,45 @@
+import { useEffect, useState } from 'react';
+
 import './SelectField.scss';
 
-const SelectField = ({ id, label, value, onChange }) => {
+const SelectField = ({ id, label, options, value, onChange }) => {
+	const [labelAside, setLabelAside] = useState(false);
+	const [isFocus, setIsFocus] = useState(false);
+
+	useEffect(() => {
+		value || isFocus ? setLabelAside(true) : setLabelAside(false);
+	}, [value, isFocus]);
+
 	return (
 		<div className="selectField">
-			<label>{label}</label>
-			<select id={id} name={id} value={value} onChange={onChange}>
-				{value.map((item) => {
-					return <option value={item.id}>{item.name}</option>;
-				})}
-			</select>
+			<div className="selectField__container">
+				<label
+					className={
+						labelAside
+							? 'selectField__label selectField__label--aside'
+							: 'selectField__label'
+					}
+					htmlFor={id}
+				>
+					{label}
+				</label>
+				<select
+					className="selectField__field"
+					id={id}
+					onChange={onChange}
+					onFocus={() => setIsFocus(true)}
+					onBlur={() => setIsFocus(false)}
+				>
+					<option value=""></option>
+					{options.map(({ id, name }) => {
+						return (
+							<option key={id} value={id}>
+								{name}
+							</option>
+						);
+					})}
+				</select>
+			</div>
 		</div>
 	);
 };
