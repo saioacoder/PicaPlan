ff93afa5db56ea9390a0362681ca5827c7268efc
Saioa <saioacoder@gmail.com>
2021-08-21 17:19:38 +0200

    Add form visibility status inside the component

--- src/pages/ingredients/components/FormAddIngredient/FormAddIngredient.jsx
+++ src/pages/ingredients/components/FormAddIngredient/FormAddIngredient.jsx
@@ -2,12 +2,17 @@ import { useState } from 'react';
 
 import { addItem } from '../../../../logic/shared';
 
+import Button from '../../../../components/Button/Button.jsx';
+import ButtonAddItem from '../../../../components/ButtonAddItem/ButtonAddItem.jsx';
 import InputField from '../../../../components/InputField/InputField.jsx';
 import SelectField from '../../../../components/SelectField/SelectField.jsx';
 
+import './FormAddIngredient.scss';
+
 const FormAddIngredient = ({ ingredientTypes, onSubmit }) => {
 	const [name, setName] = useState('');
-	const [idIngredientType, setIdIngredientType] = useState([]);
+	const [idIngredientType, setIdIngredientType] = useState('');
+	const [isAddFormOpen, setIsAddFormOpen] = useState(false);
 
 	const handleAddItem = async (e) => {
 		e.preventDefault();
@@ -20,22 +25,32 @@ const FormAddIngredient = ({ ingredientTypes, onSubmit }) => {
 	};
 
 	return (
-		<form onSubmit={handleAddItem}>
-			<InputField
-				id="name"
-				label="Nombre"
-				value={name}
-				onChange={({ target: { value } }) => setName(value)}
-			/>
-			<SelectField
-				id="idIngredientType"
-				label="Tipo de ingrediente"
-				options={ingredientTypes}
-				value={idIngredientType}
-				onChange={({ target: { value } }) => setIdIngredientType(value)}
-			/>
-			<button type="submit">Añadir</button>
-		</form>
+		<>
+			<ButtonAddItem onClick={() => setIsAddFormOpen(true)} />
+			<form
+				className={
+					isAddFormOpen
+						? 'formAddIngredient formAddIngredient--open'
+						: 'formAddIngredient'
+				}
+				onSubmit={handleAddItem}
+			>
+				<InputField
+					id="name"
+					label="Nombre"
+					value={name}
+					onChange={({ target: { value } }) => setName(value)}
+				/>
+				<SelectField
+					id="idIngredientType"
+					label="Tipo de ingrediente"
+					options={ingredientTypes}
+					value={idIngredientType}
+					onChange={({ target: { value } }) => setIdIngredientType(value)}
+				/>
+				<Button>Añadir ingrediente</Button>
+			</form>
+		</>
 	);
 };
 
