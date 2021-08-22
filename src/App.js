af62fb5bd5c31acee9e49245609fcacf849567e0
Saioa <saioacoder@gmail.com>
2021-08-22 09:17:38 +0200

    Change components location

--- src/App.js
+++ src/App.js
@@ -1,38 +1,38 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
-import firebase from 'firebase/app'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import firebase from 'firebase/app';
 
-import firebaseConfig from './config'
+import firebaseConfig from './config';
 
-import Diary from './pages/diary/diary.jsx'
-import Ingredients from "./pages/ingredients/ingredients.jsx"
-import IngredientTypes from "./pages/ingredientTypes/ingredientTypes.jsx"
-import Plates from "./pages/plates/plates.jsx"
-import PlateTypes from "./pages/plateTypes/plateTypes.jsx"
+import Diary from './pages/diary/diary.jsx';
+import Ingredients from './pages/ingredients/ingredients.jsx';
+import IngredientTypes from './pages/ingredientTypes/ingredientTypes.jsx';
+import Plates from './pages/plates/plates.jsx';
+import PlateTypes from './pages/plateTypes/plateTypes.jsx';
 
-firebase.initializeApp(firebaseConfig)
+firebase.initializeApp(firebaseConfig);
 
 function App() {
-  return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <Diary />
+	return (
+		<Router>
+			<Switch>
+				<Route exact path="/">
+					<Diary />
 				</Route>
-        <Route exact path="/ingredients/">
-          <Ingredients />
+				<Route exact path="/ingredients">
+					<Ingredients />
 				</Route>
-        <Route exact path="/ingredientTypes/">
-          <IngredientTypes />
+				<Route exact path="/ingredientTypes/">
+					<IngredientTypes />
 				</Route>
-        <Route exact path="/plates/">
-          <Plates />
+				<Route exact path="/plates/">
+					<Plates />
 				</Route>
-        <Route exact path="/plateTypes/">
-          <PlateTypes />
+				<Route exact path="/plateTypes/">
+					<PlateTypes />
 				</Route>
-      </Switch>
-    </Router>
-  )
+			</Switch>
+		</Router>
+	);
 }
 
-export default App
+export default App;
