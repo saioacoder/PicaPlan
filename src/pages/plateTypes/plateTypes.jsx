c80bfb29fce800ab52c22042f0dca8cafb0c3db3
Saioa <saioacoder@gmail.com>
2021-08-24 15:14:56 +0200

    Add logic to plateTypes page

--- src/pages/plateTypes/plateTypes.jsx
+++ src/pages/plateTypes/plateTypes.jsx
@@ -1,24 +1,37 @@
 import { useState, useEffect } from 'react';
 
-import { loadList } from '../../logic/shared';
+import { reloadList } from '../../logic/shared';
 
-import ButtonAddItem from '../../components/ButtonAddItem/ButtonAddItem.jsx';
+import FormAddPlateType from '../../components/FormAddPlateType/FormAddPlateType.jsx';
 import ItemCard from '../../components/ItemCard/ItemCard.jsx';
 import PageLayout from '../../components/PageLayout/PageLayout.jsx';
 
 const PlateTypes = () => {
 	const [plateTypes, setPlateTypes] = useState([]);
 
+	const reloadPlateTypeList = () => {
+		reloadList('plateTypes', setPlateTypes);
+	};
+
 	useEffect(() => {
-		loadList('plateTypes', setPlateTypes);
+		reloadPlateTypeList();
 	}, []);
 
 	return (
 		<PageLayout pageTitle="Tipos de platos" menuSel="plateTypes">
 			{plateTypes.map(({ id, name }) => {
-				return <ItemCard key={id} name={name} size="small" />;
+				return (
+					<ItemCard
+						key={id}
+						id={id}
+						name={name}
+						type="plateTypes"
+						size="small"
+						onClickRemove={reloadPlateTypeList}
+					/>
+				);
 			})}
-			<ButtonAddItem type="plateType" />
+			<FormAddPlateType onSubmit={reloadPlateTypeList} />
 		</PageLayout>
 	);
 };
