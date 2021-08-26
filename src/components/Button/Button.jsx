895d2ee837dcf6c7c2e4eebb5f6fec28f01f6d0b
Saioa <saioacoder@gmail.com>
2021-08-26 16:15:41 +0200

    Refactor button component

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,11 +1,11 @@
 import './Button.scss';
 
-const Button = ({ onClick, type, secondary = false, children }) => {
+const Button = ({ onClick, type = 'submit', secondary = false, children }) => {
 	return (
 		<button
 			className={secondary ? 'button button__secondary' : 'button'}
 			onClick={onClick}
-			type={type === undefined ? 'submit' : type}
+			type={type}
 		>
 			{children}
 		</button>
