480ea220a42373bef6bdd90c19b5a1c96997b68a
Saioa <saioacoder@gmail.com>
2021-09-07 16:23:02 +0200

    Created a new component for texts on formularies

---
+++ src/components/SimpleTextField/SimpleTextField.jsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from 'react';
+
+import './SimpleTextField.scss';
+
+const SimpleTextField = ({ label = '', className = '', value }) => {
+	const [labelAside, setLabelAside] = useState(false);
+	const FIELD_NAME = 'simpleTextField';
+
+	useEffect(() => {
+		value !== undefined && value !== ''
+			? setLabelAside(true)
+			: setLabelAside(false);
+	}, [value]);
+
+	return (
+		<div className={`${FIELD_NAME} ${className}`}>
+			<div className={`${FIELD_NAME}__container`}>
+				<label
+					className={
+						labelAside
+							? `${FIELD_NAME}__label ${FIELD_NAME}__label--aside`
+							: `${FIELD_NAME}__label`
+					}
+				>
+					{label}
+				</label>
+				<div class={`${FIELD_NAME}__field`}>{value}&nbsp;</div>
+			</div>
+		</div>
+	);
+};
+
+export default SimpleTextField;
