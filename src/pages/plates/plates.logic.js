e19aaa2b0927aa72fcc9e44e3b159e25a05608e8
Saioa <saioacoder@gmail.com>
2021-09-07 16:20:27 +0200

    Add getUnitName function to Plates page

--- src/pages/plates/plates.logic.js
+++ src/pages/plates/plates.logic.js
@@ -1,5 +1,10 @@
 import { reloadList, removeItem } from '../../logic/shared';
 
+export const getUnitName = (id, unitsList) => {
+	const unit = unitsList.filter((unit) => unit.id === id);
+	return unit[0].name;
+};
+
 export const handleRemove = async (id, setPlatesList, setMessageBox) => {
 	await removeItem('plates', id);
 	reloadList('plates', 'name', setPlatesList);
