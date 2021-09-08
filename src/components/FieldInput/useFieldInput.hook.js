5312c5014d8a7c57e6adc52fe020ac6610a1ce50
Saioa <saioacoder@gmail.com>
2021-09-08 10:23:39 +0200

    Fix FieldInput hook error manage issue

--- src/components/FieldInput/useFieldInput.hook.js
+++ src/components/FieldInput/useFieldInput.hook.js
@@ -1,12 +1,14 @@
 import { useState } from 'react';
 
-export default function useFieldInput(defaultValue) {
+export default function useFieldInput(defaultValue, isRequired = false) {
 	const [value, setValue] = useState(defaultValue);
+	const [error, setError] = useState(false);
 
 	const handleChange = (e) => {
-		const inputValue = e?.target?.value ?? '';
+		const inputValue = e.target.value;
 		setValue(inputValue);
+		isRequired && inputValue === '' ? setError(true) : setError(false);
 	};
 
-	return [value, setValue, handleChange];
+	return [value, setValue, handleChange, error];
 }
